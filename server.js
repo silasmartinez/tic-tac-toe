87ccfa6573dff633f8df2a4ab29350276022a7e7
silasmartinez <silasm@gmail.com>
2015-06-18 22:25:16 -0600

    Server set up, Procfile added...

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 /**
  * Created by silasmartinez on 6/18/15.
  */
+var http = require('http'),
+  fs = require('fs'),
+  mime = require('mime')
+
+var server = http.createServer(function (req, res) {
+  reqUrl = req.url === '/' ? './index.html' : '.' + req.url
+  mime.default_type = 'text-html'
+  fs.readFile(reqUrl, function (err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'})
+      res.write('404 Not Found\n')
+      return
+    }
+    res.setHeader('Content-Type', mime.lookup(req.url))
+    res.end(data)
+  })
+
+})
+
+var port =  process.env.PORT || 9001
+server.listen(port)
