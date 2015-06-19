26ad8016542e9b313d59e3c64fcb888df762faf8
silasmartinez <silasm@gmail.com>
2015-06-19 09:54:11 -0600

    Refactored, mostly instanced, WAY over designed.....

--- client.js
+++ client.js
@@ -6,6 +6,7 @@ client = {
   claimBlock: function (players, block, event) {
     if (players.indexOf(block) === -1) {
       if (event) {
+        game.board[event.target.dataset.id] = game.activePlayer
         event.srcElement.innerHTML = game.activePlayer
       }
       return true
@@ -13,35 +14,40 @@ client = {
       return false
     }
   },
-  resetGame: function () {
+  paintGame: function (myBoard) {
     var blocks = grid.getElementsByTagName('div')
     for (var i = 0; i < blocks.length; i++) {
-      blocks[i].innerHTML = ''
+      _position = i + 1
+      _position = '' + _position
+      blocks[i].innerHTML = myBoard[_position]
     }
-    game.activePlayer = 'x'
-    game.turnCount = 0
-    players.o = []
-    players.x = []
-    next.innerHTML = 'Next Up: X'
-
+    next.innerHTML = 'Next Up: ' + game.activePlayer.toUpperCase()
   }
 }
 
+var players = new Players()
+var game = new Game()
 var grid = document.getElementById('grid'),
   next = document.getElementById('nextUp')
 
 grid.onclick = function (e) {
   if (e.target.dataset.id) {
-    console.log(e.target.dataset.id)
+    //console.log(e.target.dataset.id)
     client.claimBlock(players.getBoth(), e.target.dataset.id, e)
     players.addBlock(game.activePlayer, e.target.dataset.id)
-    if (game.isWinner(players[game.activePlayer])){
+
+    if (game.isWinner(players)){
       alert(game.activePlayer.toUpperCase() + ' is the winner!')
-      client.resetGame()
+      game = new Game()
+      players = new Players()
+      client.paintGame(game.board)
     } else if (game.turnCount === 9){
+      game = new Game()
+      players = new Players()
+      client.paintGame(game.board)
       alert('Draw!')
-      client.resetGame()
+    } else {
+      game.newTurn()
     }
-    game.newTurn()
   }
 }
--- game.js
+++ game.js
@@ -4,53 +4,69 @@ var first = function () {
   return 'result'
 }
 
-var players = {
-  x: [],
-  o: [],
-  getBoth: function () {
-    return this.x.concat(this.y)
-  },
-  addBlock: function (player, block) {
-    this[player].push(parseInt(block))
-  }
+function Players () {
+  this.x = []
+  this.o = []
 }
 
-var game = {
-  activePlayer: 'x',
-  turnCount: 0,
-  winningCombos: [[1, 2, 3], [4, 5, 6],
+Players.prototype.getBoth = function () {
+  return this.x.concat(this.y)
+}
+
+Players.prototype.addBlock = function (player, block) {
+  this[player].push(parseInt(block))
+}
+
+function Game () {
+  this.activePlayer = 'x'
+  this.turnCount = 0
+  this.winningCombos = [[1, 2, 3], [4, 5, 6],
     [7, 8, 9], [1, 4, 7], [2, 5, 8],
-    [3, 6, 7], [1, 5, 9], [3, 5, 7]],
-  isWinner: function () {
-    var player = players[this.activePlayer]
-    console.log('checking for winner: ' + this.activePlayer + ': ' + player  )
-    var won = false
-    this.winningCombos.forEach(function (ele) {
-      if (won) {return}
-      var count = 0
-      ele.forEach(function (intEle) {
-        if (player.indexOf(intEle) !== -1) {
-          count++
-        }
-        if (count === 3) {
-          won = true
-          return
-        }
-      })
-    })
-    return won
-  },
-  newTurn: function () {
-    if (this.activePlayer === 'x'){
-      this.activePlayer = 'o'
-    } else {
-      this.activePlayer = 'x'
-    }
-    next.innerHTML = 'Next Up: ' + this.activePlayer.toUpperCase()
-    this.turnCount++
+    [3, 6, 7], [1, 5, 9], [3, 5, 7]]
+  this.board = {
+    '1' : '',
+    '2' : '',
+    '3' : '',
+    '4' : '',
+    '5' : '',
+    '6' : '',
+    '7' : '',
+    '8' : '',
+    '9' : '',
+    nextUp: 'X'
   }
 }
 
+Game.prototype.isWinner = function (players) {
+  var player = players[this.activePlayer]
+  console.log('checking for winner: ' + this.activePlayer + ': ' + player)
+  var won = false
+  this.winningCombos.forEach(function (ele) {
+    if (won) {return}
+    var count = 0
+    ele.forEach(function (intEle) {
+      if (player.indexOf(intEle) !== -1) {
+        count++
+      }
+      if (count === 3) {
+        won = true
+        return
+      }
+    })
+  })
+  return won
+}
+
+Game.prototype.newTurn = function () {
+  if (this.activePlayer === 'x') {
+    this.activePlayer = 'o'
+  } else {
+    this.activePlayer = 'x'
+  }
+  this.board.nextUp = this.activePlayer.toUpperCase()
+  client.paintGame(this.board)
+  this.turnCount++
+}
 
 //export block here
 module.exports = {
